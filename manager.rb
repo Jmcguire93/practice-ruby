a211efb831a8d0130678c06dd8f4da4b02c9c7f2
James <james.ryan.mcguire@gmail.com>
2021-06-10 13:39:00 -0700

    Adds Manager class and options hash to Employee

---
+++ manager.rb
@@ -0,0 +1,64 @@
+class Employee
+
+  def initialize(input_options)
+    @first_name = input_options[:first_name]
+    @last_name = input_options[:last_name]
+    @salary = input_options[:salary]
+    @active = input_options[:active]
+  end
+  
+  
+  def print_info
+    puts "#{@first_name} #{@last_name} makes $#{@salary} a year."
+  end
+  
+  def first_name
+    puts @first_name
+  end
+
+  def last_name
+    puts @last_name
+  end
+
+  def active
+    puts @active
+  end
+
+  def active=(active)
+    @active = active
+  end
+
+  def give_annual_raise
+    @salary = 1.05 * @salary
+  end
+
+end
+
+employee1 = Employee.new({first_name: "Majora", last_name: "Carter", salary: 80000, active: true})
+employee2 = Employee.new({first_name: "Danilo", last_name: "Campos", salary: 70000, active: true})
+
+employee1.print_info
+employee1.give_annual_raise
+employee1.print_info
+
+employee2.print_info
+employee2.give_annual_raise
+employee2.print_info
+
+employee1.active 
+employee1.active = false        ## employee1.active=(false)
+employee1.active
+
+
+class Manager < Employee
+
+  def send_report
+    puts "Sending email..."
+    # use email sending library
+    puts "Email sent!"
+  end
+end
+
+manager = Manager.new({first_name: "Sharon", last_name: "Yitbarek", salary: 100000, active: true})
+manager.print_info
+manager.send_report
