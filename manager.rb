685126ce11282ea3b26cb6a119038c297eca654e
James <james.ryan.mcguire@gmail.com>
2021-06-10 14:57:27 -0700

    Adds give_all_raises method

--- manager.rb
+++ manager.rb
@@ -40,12 +40,12 @@ employee1 = Employee.new({first_name: "Majora", last_name: "Carter", salary: 800
 employee2 = Employee.new({first_name: "Danilo", last_name: "Campos", salary: 70000, active: true})
 
 employee1.print_info
-employee1.give_annual_raise
-employee1.print_info
+#employee1.give_annual_raise
+#employee1.print_info
 
 employee2.print_info
-employee2.give_annual_raise
-employee2.print_info
+#employee2.give_annual_raise
+#employee2.print_info
 
 employee1.active 
 employee1.active = false        ## employee1.active=(false)
@@ -69,8 +69,22 @@ class Manager < Employee
     # use email sending library
     puts "Email sent!"
   end
+
+  def give_all_raises
+    i = 0
+    while i < @employees.length
+      @employees.each do |employee|
+        employee.give_annual_raise
+      end
+      i += 1
+    end
+    #puts @salary
+  end
 end
 
 manager = Manager.new({first_name: "Sharon", last_name: "Yitbarek", salary: 100000, active: true, employees: [employee1, employee2]})
 manager.print_info
 manager.send_report
+manager.give_all_raises
+p employee1.salary
+employee1.print_info
